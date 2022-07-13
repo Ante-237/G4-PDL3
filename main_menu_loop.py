04346965944eff7d1bc2d1d2d642bf1049d6e758
Ante-237 <akumawahanye@gmail.com>
2022-07-13 10:56:50 +0200

    Skeleton of MAIN MENU LOOP

--- main_menu_loop.py
+++ main_menu_loop.py
@@ -1,30 +1,34 @@
 
 
 def loop_main_menu():
-#   show menu options
+    # show menu options
     user_input = int(input("Enter the Menu option:"))
     while user_input != 5:
-#   show menu options
+        # show menu options
         if user_input == 1:
-#   call secret rooms function
+            print("TODO")
+            # call secret rooms function
         elif user_input == 2:
-# call positive vibes room function
+            print("TODO")
+            # call positive vibes room function
         elif user_input == 3:
-# call gallery not available show function
-# draw the entire same screen again
+            print("TODO")
+            # call gallery not available show function
+            # draw the entire same screen again
         elif user_input == 4:
-
-# call lgame room not avaible function
-# draw the entire same screen again
+            print("TODO")
+            # call lgame room not available function
+            # draw the entire same screen again
         elif user_input not in range(0, 6):
+            print("TODO")
 
-# call function for invalid options
-# draw the entire same screen again
+            # call function for invalid options
+            # draw the entire same screen again
 
         # get user input again
     user_input = int(input("Enter the Menu option:"))
 
-    #go to TOP MENU IS exit is entered
+    # go to TOP MENU IS exit is entered
 
 
 
