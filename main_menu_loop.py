8ab9fa8bfc05c77b0473344c799c128d5ac982fd
Ante-237 <akumawahanye@gmail.com>
2022-07-13 12:02:20 +0200

    Added Skeleton for secret room menu

--- main_menu_loop.py
+++ main_menu_loop.py
@@ -31,4 +31,29 @@ def loop_main_menu():
     # go to TOP MENU IS exit is entered
 
 
+def secret_room_menu():
+    # show menu options
+    user_input = int(input("Enter the Menu options: "))
+    while user_input != 3:
+        print("TODO")
+        if user_input == 1:
+            print("TODO")
+            # call function to get a random word from the read list and output to the screen
+        if user_input == 2:
+            print("TODO")
+            # call function that ask user to add their story and adds it to the list
+        # show options
+        user_input = int(input("Enter the Menu options: "))
+
+    # go to MAIN MENU if exit value is entered
+
+
+
+            # call function
+
+
+
+
+
+
 
