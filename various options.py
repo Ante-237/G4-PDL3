334f4b6ca35b108a7957a4fa1df7e8f232588f43
isaac.rudasingwa <rudasaacv@gmail.com>
2022-07-13 13:50:55 +0200

    codes to print various options.

---
+++ various options.py
@@ -0,0 +1,8 @@
+#creating a function
+def my_function():
+    #printing various options
+    print("Select an option")
+    print("1-Menu")
+    print("2-About")
+    print("3-Privacy")
+    print("4-Exit")
