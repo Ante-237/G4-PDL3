a0e3363da084703475ab2f4ede7f2c3054f75dee
isaac.rudasingwa <rudasaacv@gmail.com>
2022-07-13 16:26:25 +0200

    codes to print the main menu options

--- Main menu.py
+++ Main menu.py
@@ -1,9 +1,12 @@
 def my_function1():
-    print("Select an option")
-    print("1-Secret Rooms")
-    print("2-Positive Vibes")
-    print("3-Art Gallery")
-    print("4-Game Room")
-    print("5-Exit")
+    print(" ____________________")
+    print("|  Select an option  |")
+    print("|  1-Secret Rooms    |")
+    print("|  2-Positive Vibes  |")
+    print("|  3-Art Gallery     |")
+    print("|  4-Game Room       |")
+    print("|  5-Exit            |")
+    print(" --------------------")
 
 
+my_function1()
