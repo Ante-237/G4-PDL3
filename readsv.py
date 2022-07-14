d838d7c3a7c367f71d32adaea66c1dfa2dfd31bf
SugiraVinc <sugiravincentg11@gmail.com>
2022-07-14 12:10:50 +0200

    Both read and write/append functions

--- readsv.py
+++ readsv.py
@@ -1,5 +1,18 @@
 # A function that reads a file and returns each line of the file
 def read():
     read_file = open("SecreteVibes.txt", mode='r', encoding='utf-8')
-    for i in read_file:
-        print(read_file, end="")
+    get_file = read_file.readline()
+    print("\t\t---------------------------------------------")
+    print(get_file)
+    print("\t\t---------------------------------------------")
+
+
+# A function that appends a file
+
+def wt_vibes():
+    vibe_bank = open("SecreteVibes.txt", mode='a')
+    my_vibe = input(str("Please tell us your vibe: "))
+    vibe_bank.write(my_vibe)
+
+
+
