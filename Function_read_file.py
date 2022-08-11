6e9c619c51898bda65b236dba8762667222c33ce
Gatwaza <Gatwazarobert177@gmail.com>
2022-08-11 02:38:50 +0200

    Function to read and display from file

--- Function_read_file.py
+++ Function_read_file.py
@@ -1,7 +1,7 @@
 # Function to read and respond visually
 
 file = open('PositiveVibes.txt', 'r', encoding='utf-8')
-p = ["World is luck to have you\n","You got this:)","Lovely\n","you are\n"]
-        file.write("Hey\n")
-        file.writelines(p)
-        file.close()
+p = ["Wrold is luck to have you\n", "You got this:)", "Lovely you are"]
+file.write("Hey\n")
+file.writelines(p)
+file.close()
