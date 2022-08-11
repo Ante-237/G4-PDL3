6039a783b6025365e6c6f576b98f86548166b019
root <o.olurinola@alustudent.com>
2022-08-11 10:36:20 +0200

    fix:))

--- stephen_function.py
+++ stephen_function.py
@@ -3,5 +3,12 @@ def exit_function():
         print("Bye! and remember to take care of yourself.")
 
 
-def unavailable()
-	print("The option you are trying to choose is currently unavailable")
+def unavailable():
+    print("The option you are trying to choose is currently unavailable"
+
+
+def NotAvailable(name):
+    print("Hope you're leaving better than you came " + name + ", come again soon")
+
+
+
