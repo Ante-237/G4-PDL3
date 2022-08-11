d3316809d8b3f22474de5c0b905bc7e1c0193b1e
Stephen30o0 <106529980+Stephen30o0@users.noreply.github.com>
2022-08-11 11:22:37 +0200

    fixx//

--- stephen_function.py
+++ stephen_function.py
@@ -1,13 +1,12 @@
 def exit_function():
-    if input() == "exit":
-        print("Bye! and remember to take care of yourself.")
+    print("Bye! and remember to take care of yourself.")
 
 
 def unavailable():
     print("The option you are trying to choose is currently unavailable"
 
 
-def NotAvailable(name):
+def not_available(name):
     print("Hope you're leaving better than you came " + name + ", come again soon")
 
 
