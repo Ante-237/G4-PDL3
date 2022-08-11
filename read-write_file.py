956a064e787e05e1c23e4482eb6150d6a5bae679
Ante-237 <akumawahanye@gmail.com>
2022-08-11 09:51:32 +0200

    taking entry , update to read-write-file.py

--- read-write_file.py
+++ read-write_file.py
@@ -1,11 +1,12 @@
 #A function that displays the SecretVibes title
+import random
+
 
 def read_title():
     print(" This is the Secrete Vibes Room ")
 
 # A function that reads a file and returns each line of the file
 #
-import random
 
 
 def read():
@@ -20,8 +21,11 @@ def read():
     print("\t\t---------------------------------------------")
 
 
-# A function that appends a file
-def wt_vibes():
-    vibe_bank = open("SecreteVibes.txt", mode='a')
-    my_vibe = input(str("Please tell us your vibe: "))
-    vibe_bank.write(my_vibe)
+# A function that appends a file secret vibes
+def wt_vibes(entry):
+    # vibe_bank = open("SecreteVibes.txt", mode='a')
+    # my_vibe = input(str("Please tell us your vibe: "))
+    # vibe_bank.write(my_vibe)
+    with open("SecreteVibes.txt", "a", encoding='utf-8') as file:
+        file.write("\n"+entry)
+
