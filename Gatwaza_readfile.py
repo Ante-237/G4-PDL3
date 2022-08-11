e097ec6d9a45bf791970576f826a373d4c41aec4
Stephen30o0 <106529980+Stephen30o0@users.noreply.github.com>
2022-08-11 12:24:33 +0200

    update to file

--- Gatwaza_readfile.py
+++ Gatwaza_readfile.py
@@ -2,11 +2,14 @@
 import random
 
 # get message from positive vibes
+class PositiveMessage:
 
-
-def read_positive_vibes():
-    with open("PositiveVibes.txt", "r", encoding='utf-8') as file:
-        data = file.readline()
-        print("\t\t---------------------------------------------")
-        print(random.choice(data))
-        print("\t\t---------------------------------------------")
+    def __init__(self):
+        pass
+    
+    def read_positive_vibes(self):
+        with open("PositiveVibes.txt", "r", encoding='utf-8') as file:
+            data = file.readline()
+            print("\t\t---------------------------------------------")
+            print(random.choice(data))
+            print("\t\t---------------------------------------------")
