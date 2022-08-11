19980bea3815362514bd19cafd3378359b6f683f
Ante-237 <akumawahanye@gmail.com>
2022-08-11 12:17:37 +0200

    update to Princess_Functions.py

--- Princess_Functions.py
+++ Princess_Functions.py
@@ -1,38 +1,34 @@
 # Name of app function:
 # This function displays the name of the app
 
-name = "** LUMISPACE **"
+class HeadingPart:
+
+    name = "** LUMISPACE **"
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def display_name_of_app(self):
+        print("\t\t", self.name)
+    # Menu Title Function:
+    # The function displays the Title Menu
+
+    @staticmethod
+    def main_menu_title(self):
+        print("\t\t\t - MAIN MENU -")
+
+    # Function to exit
+    # The function displays a message to the user that they are exiting the secret room
+
+    @staticmethod
+    def function_to_exit(self):
+        print("\t\t - You are exiting the secret room.")
+
+    # Title Screen Function
+    # The function will display the title of the secret room
+    @staticmethod
+    def function_for_pv_title(self):
+        print("- The Positive Vibes Screen -")
 
 
-def display_name_of_app():
-    print("\t\t", name)
-
-
-display_name_of_app()
-
-# Menu Title Function:
-# The function displays the Title Menu
-
-def main_menu_title():
-    print("\t\t\t - MAIN MENU -")
-
-
-main_menu_title()
-
-# Function to exit
-# The function displays a message to the user that they are exiting the secret room
-
-def function_to_exit():
-    print("\t\t - You are exiting the secret room.")
-
-
-function_to_exit()
-
-# Title Screen Function
-# The function will display the title of the secret room
-
-def function_for_pv_title():
-    print("- The Positive Vibes Screen -")
-
-
-function_for_pv_title()
