31f57d1b9a5c0fa4500b7a847e2408580fb97b06
root <o.olurinola@alustudent.com>
2022-08-11 10:08:20 +0200

    fixes://:

---
+++ stephen_function.py
@@ -0,0 +1,7 @@
+def exit_function():
+    if input() == "exit":
+        print("Bye! and remember to take care of yourself.")
+
+
+def unavailable()
+	print("The option you are trying to choose is currently unavailable")
