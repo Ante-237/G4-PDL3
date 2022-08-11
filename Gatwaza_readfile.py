aec2b642f61f695ac8b1d4df6d20123e9dfd31e0
Gatwaza <Gatwazarobert177@gmail.com>
2022-08-11 11:21:09 +0200

    function for user inputs and read file

--- Gatwaza_readfile.py
+++ Gatwaza_readfile.py
@@ -3,3 +3,13 @@ import random
 
 # print random word
 print(random.choice(open("Gatwaza_PositiveVibes.txt", "r").readline().split()))
+
+# take two values from user
+name = input("Enter your Name: ")
+Positive_room = input("Enter any positive word: ")
+
+# Display all values on screen
+print("\n")
+print("Printing Individual Details")
+print("Name", "Positive_room")
+print(name, Positive_room)
