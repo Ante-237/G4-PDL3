c73be8cdb5b711c5a18fc6b881c454ccb87313cd
Stephen30o0 <106529980+Stephen30o0@users.noreply.github.com>
2022-08-11 11:40:19 +0200

    oop conversion

--- stephen_function.py
+++ stephen_function.py
@@ -1,13 +1,17 @@
-def exit_function():
-    print("Bye! and remember to take care of yourself.")
+class Messages:
+    
+    def __init__(self):
+
+    def exit_function(self):
+        print("Bye! and remember to take care of yourself.")
 
 
-def unavailable():
-    print("The option you are trying to choose is currently unavailable"
+    def unavailable(self):
+        print("The option you are trying to choose is currently unavailable"
 
 
-def not_available(name):
-    print("Hope you're leaving better than you came " + name + ", come again soon")
+    def not_available(self, name):
+        print("Hope you're leaving better than you came " + name + ", come again soon")
 
 
 
