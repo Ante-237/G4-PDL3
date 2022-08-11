7521e552fed50514cab442b1c73621e7da52a518
Ante-237 <akumawahanye@gmail.com>
2022-08-11 11:02:06 +0200

    update to isaac branch

--- Main menu.py
+++
@@ -1,9 +0,0 @@
-def my_function1():
-    print(" ____________________")
-    print("|  Select an option  |")
-    print("|  1-Secret Rooms    |")
-    print("|  2-Positive Vibes  |")
-    print("|  3-Art Gallery     |")
-    print("|  4-Game Room       |")
-    print("|  5-Exit            |")
-    print(" --------------------")
--- Secretroom options.py
+++
@@ -1,8 +0,0 @@
-def my_function2():
-    #printing secret room options
-    print(" __________________")
-    print("|  Select an option | ")
-    print("|  1-Read Word      |")
-    print("|  2-Add Your Story |")
-    print("|  3-Exit           |")
-    print(" ------------------ ")
