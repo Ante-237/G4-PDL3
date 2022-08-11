18dc6ba11868d7e252ffdc091371378d657357e7
Ante-237 <akumawahanye@gmail.com>
2022-08-11 12:29:22 +0200

    update Gatwaza_readfile.py

--- Gatwaza_readfile.py
+++ Gatwaza_readfile.py
@@ -2,11 +2,14 @@
 import random
 
 # get message from positive vibes
+
+
 class PositiveMessage:
 
     def __init__(self):
         pass
-    
+
+    @staticmethod
     def read_positive_vibes(self):
         with open("PositiveVibes.txt", "r", encoding='utf-8') as file:
             data = file.readline()
